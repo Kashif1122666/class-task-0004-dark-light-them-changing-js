e1cbd9260e322e4ae5d9b27cf3ec320ff2b6a836
kashif1122666 <kashif1122666@gmail.com>
2024-09-14 22:56:25 -0700

    minor changes

--- script.js
+++ script.js
@@ -2,21 +2,15 @@ var btn = document.getElementById('button');
 var body = document.getElementById('body');
 function changeThem() {
     if(btn.innerText === 'Night'){
-        // body.style.backgroundColor='black'
+        body.style.backgroundColor='black'
         btn.innerText='Day'
-        // btn.style.backgroundColor='white'
-        // btn.style.color='black'
-        body.classList.add('styled1');
-        btn.classList.add('styled2');
+        btn.style.backgroundColor='white'
+        btn.style.color='black'
     }
     else{
-        // body.style.backgroundColor='white';
+        body.style.backgroundColor='white';
         btn.innerText='Night'
-        // btn.style.backgroundColor='black'
-        // btn.style.color='white'
-        body.classList.add('styled3');
-        btn.classList.add('styled4');
-
-
+        btn.style.backgroundColor='black'
+        btn.style.color='white'
     }
 }
